7fc0118c428fa891237d22abf1f3353693cd951c
m1kqn <137014023+suzuki689@users.noreply.github.com>
2025-04-15 02:03:53 +0000

    たぶんできました

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,7 +9,11 @@
     <div class="login-box">
       <h2>
 <?php
+  $indata = filter_input(INPUT_GET,"indata");
 
+  if(mb_ereg('^[0-9]+$',$indata)==false){
+    die("数値を入力してください");
+  }
 
 echo "入力された数字は： " . $_GET['indata'];
 ?>
--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,6 +9,24 @@
     <div class="login-box">
       <h2>
 <?php
+
+$username = filter_input(INPUT_GET,'username');
+$useraddress = filter_input(INPUT_GET,'useraddress');
+$usermail = filter_input(INPUT_GET,'usermail');
+
+if (mb_ereg('^[ぁ-んァ-ヶー-龠々]{20}+$',$username)==false){
+  die("20文字までの日本語で入力してください。");
+}
+
+if (mb_ereg('^[ぁ-んァ-ヶー-龠々]{50}+$',$useraddress)==false){
+  die("50文字までの日本語で入力してください。");
+}
+
+if (mb_ereg('^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$',$usermail)==false){
+  die("100文字までで入力してください。");
+}
+
+
 echo "あなたが入力した値<br>";
 echo "名前：" . $_GET['username'] . "<br>";
 echo "住所：" . $_GET['useraddress']. "<br>";
